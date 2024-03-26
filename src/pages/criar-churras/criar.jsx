c8462e2eda64dbdcd5ef297d764c131a97b5596c
arthurzop <medeirosartur48@gmail.com>
2024-03-26 20:18:01 -0300

    feat: initial screen finished and trying to do the create xurras screen

--- src/pages/criar-churras/criar.jsx
+++ src/pages/criar-churras/criar.jsx
@@ -1,48 +1,35 @@
 import * as S from "./style.jsx";
 import Header from "../../components/header/header";
 import Button from "../../components/button/button";
+import { useState } from "react";
+import { TextInput } from "react-native";
 
 export default function Criar() {
+//   const valorInicial = [{ id: "homens", value: 1 }];
+//   const [numHomens, setNumHomens] = useState(valorInicial);
+//   const numeroMaximo = [{ id: "homens", label: "Homens:", min: 0, max: 20 }];
   return (
     <S.Body>
       <Header />
       <Button label="Criar" />
-      {/* <S.CriarContainer>
-        <S.SubContainer>
-          <S.Input>Nome do Xurras</S.Input>
+      <S.CriarContainer>
+        <S.Container>
+          <S.Input placeholder="Nome do Xurras"></S.Input>
           <S.SubTitle>Quantidade de Pessoas</S.SubTitle>
-          <S.Container>
+          <S.SubContainer>
             <S.Column>
-              <S.Text>Homens</S.Text>
-              <S.NumberInput></S.NumberInput>
+              <S.Label>Homens:</S.Label>
+              <TextInput></TextInput>
             </S.Column>
             <S.Column>
-              <S.Text>Homens</S.Text>
-
-              <S.NumberInput></S.NumberInput>
+              <S.Label>Homens:</S.Label>
             </S.Column>
             <S.Column>
-              <S.Text>Homens</S.Text>
-
-              <S.NumberInput></S.NumberInput>
+              <S.Label>Homens:</S.Label>
             </S.Column>
-          </S.Container>
-          <S.Divider />
-          <S.Container>
-            <S.Title>i Cortes</S.Title>
-            <S.CheckboxContainer>
-              <S.Checkbox />
-              <S.Checkbox />
-              <S.Checkbox />
-              <S.Checkbox />
-              <S.Checkbox />
-              <S.Checkbox />
-              <S.Checkbox />
-              <S.Checkbox />
-            </S.CheckboxContainer> 
-          </S.Container>
-        </S.SubContainer>
-      </S.CriarContainer> */}
+          </S.SubContainer>
+        </S.Container>
+      </S.CriarContainer>
     </S.Body>
   );
 }
