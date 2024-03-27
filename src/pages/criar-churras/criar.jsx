475549024def4f012f38b68184112255bce19979
unknown <luccas.rodrigues.silva.2312@gmail.com>
2024-03-27 14:15:59 -0300

    [FEAT] Started to do the bbq page, searching a checkbox lib that works to use in it

--- src/pages/criar-churras/criar.jsx
+++ src/pages/criar-churras/criar.jsx
@@ -1,32 +1,47 @@
 import * as S from "./style.jsx";
 import Header from "../../components/header/header";
 import Button from "../../components/button/button";
-import { useState } from "react";
-import { TextInput } from "react-native";
+// import { CheckBox } from '@rneui/themed';
+import { Check } from "iconoir-react-native";
 
 export default function Criar() {
-//   const valorInicial = [{ id: "homens", value: 1 }];
-//   const [numHomens, setNumHomens] = useState(valorInicial);
-//   const numeroMaximo = [{ id: "homens", label: "Homens:", min: 0, max: 20 }];
+  //   const valorInicial = [{ id: "homens", value: 1 }];
+  //   const [numHomens, setNumHomens] = useState(valorInicial);
+  //   const numeroMaximo = [{ id: "homens", label: "Homens:", min: 0, max: 20 }];
   return (
     <S.Body>
       <Header />
       <Button label="Criar" />
       <S.CriarContainer>
         <S.Container>
-          <S.Input placeholder="Nome do Xurras"></S.Input>
+          <S.Input
+            placeholder="Nome do Xurras"
+            maxLength={20} />
           <S.SubTitle>Quantidade de Pessoas</S.SubTitle>
           <S.SubContainer>
             <S.Column>
               <S.Label>Homens:</S.Label>
-              <TextInput></TextInput>
+              <S.InputNumber
+                maxLength={2}
+                placeholder="0" placeholderTextColor='#ef233c' />
             </S.Column>
             <S.Column>
-              <S.Label>Homens:</S.Label>
+              <S.Label>Mulheres:</S.Label>
+              <S.InputNumber
+                maxLength={2}
+                placeholder="0" placeholderTextColor='#ef233c' />
+
             </S.Column>
             <S.Column>
-              <S.Label>Homens:</S.Label>
+              <S.Label>Crianças:</S.Label>
+              <S.InputNumber
+                maxLength={2}
+                placeholder="0" placeholderTextColor='#ef233c' />
             </S.Column>
+          </S.SubContainer>
+          <S.Divider />
+          <S.SubContainer>
+            <S.SubTitle>Cortes</S.SubTitle>
           </S.SubContainer>
         </S.Container>
       </S.CriarContainer>
